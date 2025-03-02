e81a2dd4241af5418b29e4450a532f78486356ef
RealAlphaUA <realalpha.ua@gmail.com>
2025-03-03 00:26:46 +0100

    refactor: replace mutable maps with ConcurrentHashMap for thread safety

--- src/main/kotlin/gg/levely/system/mogeki/ComponentCodecRepository.kt
+++ src/main/kotlin/gg/levely/system/mogeki/ComponentCodecRepository.kt
@@ -1,38 +1,43 @@
 package gg.levely.system.mogeki
 
+import java.util.concurrent.ConcurrentHashMap
+
 class ComponentCodecRepository {
 
-    private val codecs = mutableMapOf<Key<out Component>, ComponentCodec<out Component, Any>>()
-    private val keyByName = mutableMapOf<String, Key<out Component>>()
+    private val codecs = ConcurrentHashMap<Key<out Component>, ComponentCodec<out Component, Any>>()
+    private val keyByName = ConcurrentHashMap<String, Key<out Component>>()
 
     fun <T : Component> register(key: Key<T>, codec: ComponentCodec<T, *>) {
-        require(key.value != Component::class) { "Key must be a subclass of Component" }
+        require(!Component::class.java.isAssignableFrom(key.value.java)) { "Key must be a subclass of Component" }
 
         codecs[key] = codec as ComponentCodec<out Component, Any>
         keyByName[key.name] = key
     }
 
-    fun <T : Component> getCodec(key: Key<T>): ComponentCodec<T, Any>? {
+    fun <T : Component> getCodec(key: Key<T>): ComponentCodec<T, Any> {
         return codecs[key] as? ComponentCodec<T, Any>
+            ?: throw NoSuchElementException("No codec found for key: ${key.name}")
     }
 
-    fun getCodecByName(name: String): ComponentCodec<Component, Any>? {
-        return keyByName[name]?.let { key ->
-            codecs[key]
-        } as? ComponentCodec<Component, Any>
+    fun getCodecByName(name: String): ComponentCodec<Component, Any> {
+        val key = keyByName[name]
+            ?: throw NoSuchElementException("No key found for name: $name")
+        return codecs[key] as? ComponentCodec<Component, Any>
+            ?: throw NoSuchElementException("No codec found for key: $name")
     }
 
-    fun <T : Component> getKey(name: String): Key<T>? {
+    fun <T : Component> getKey(name: String): Key<T> {
         return keyByName[name] as? Key<T>
+            ?: throw NoSuchElementException("No key found for name: $name")
     }
 
-
+    fun <T : Component> unregister(key: Key<T>) {
+        codecs.remove(key)
+        keyByName.values.removeIf { it == key }
+    }
 }
 
 interface ComponentCodec<I : Component, O> {
-
     fun marshal(component: I): O
-
     fun unMarshal(output: O): I
-
 }
