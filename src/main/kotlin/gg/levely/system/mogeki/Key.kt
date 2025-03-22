084569d92abb58b54d50e5ebb03a35a25b5b25ec
RealAlphaUA <realalpha.ua@gmail.com>
2025-03-22 10:25:06 +0100

    feat: add specific key functions for various component types in Key.kt

--- src/main/kotlin/gg/levely/system/mogeki/Key.kt
+++ src/main/kotlin/gg/levely/system/mogeki/Key.kt
@@ -1,6 +1,13 @@
 package gg.levely.system.mogeki
 
 
+import gg.levely.system.mogeki.component.BooleanComponent
+import gg.levely.system.mogeki.component.ByteArrayComponent
+import gg.levely.system.mogeki.component.DoubleComponent
+import gg.levely.system.mogeki.component.FloatComponent
+import gg.levely.system.mogeki.component.IntComponent
+import gg.levely.system.mogeki.component.LongComponent
+import gg.levely.system.mogeki.component.StringComponent
 import kotlin.reflect.KClass
 
 interface Key<T : Component> {
@@ -17,3 +24,17 @@ class KeyImpl<T : Component> (
 inline fun <reified T : Component> key(name: String): Key<T> {
     return KeyImpl(name, T::class)
 }
+
+fun keyString(name: String): Key<StringComponent> = key(name)
+
+fun keyBoolean(name: String): Key<BooleanComponent> = key(name)
+
+fun keyInt(name: String): Key<IntComponent> = key(name)
+
+fun keyLong(name: String): Key<LongComponent> = key(name)
+
+fun keyFloat(name: String): Key<FloatComponent> = key(name)
+
+fun keyDouble(name: String): Key<DoubleComponent> = key(name)
+
+fun keyByteArray(name: String): Key<ByteArrayComponent> = key(name)
