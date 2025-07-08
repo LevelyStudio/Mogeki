ff5b8aedfe6d952081876157ecf46504bb17928d
RealAlphaUA <realalpha.ua@gmail.com>
2025-07-08 23:22:21 +0200

    feat: implement equals and hashCode methods for Entity class

--- src/main/kotlin/gg/levely/system/mogeki/Entity.kt
+++ src/main/kotlin/gg/levely/system/mogeki/Entity.kt
@@ -31,4 +31,18 @@ class Entity {
         return components.toMap()
     }
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Entity
+
+        return components == other.components
+    }
+
+    override fun hashCode(): Int {
+        return components.hashCode()
+    }
+
+
 }
